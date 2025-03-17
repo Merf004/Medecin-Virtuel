950d3466bc314231325edf758037be66bb36f6e3
MerF <fedimpmerlin@gmail.com>
2025-03-17 18:08:27 +0100

    Secondaire is OK

--- src/components/Secondaire.jsx
+++ src/components/Secondaire.jsx
@@ -2,11 +2,28 @@ import React from "react";
 
 function Secondaire(){
       
-      return <div class="container">
-      <div class="row">
-          <div class="col-sm-6 col-md-4">Colonne 1</div>
-          <div class="col-sm-6 col-md-4">Colonne 2</div>
-          <div class="col-sm-12 col-md-4">Colonne 3</div>
+      return <div className="container">
+      <div className="row">
+        <div className="col">
+          <img src="src\assets\Secondaire\4117025.jpg" class="d-block w-100" alt="..."/>
+          <h2 className='text-success' style={{textAlign:"center"}}>Rapport d'analyse</h2>
+          <p style={{textAlign:"center"}}>Génération des rapports d'analyses détaillés apès chaque analyse</p>
+        </div>
+        <div className="col">
+          <img src="src\assets\Secondaire\3675555.jpg" className="d-block w-100" alt="..."/>
+          <h2 className='text-success' style={{textAlign:"center"}}>Envois de notifications</h2>
+          <p style={{textAlign:"center"}}>Envois de notification par email aux utilisateurs après analyse</p>
+        </div>
+        <div className="col">
+        <img src="src\assets\Secondaire\9907664.jpg" className="d-block w-100" alt="..."/>
+        <h2 className='text-success' style={{textAlign:"center"}}>Collecte de données</h2>
+          <p style={{textAlign:"center"}}>Collecte des données sur patients</p>
+        </div>
+        <div className="col">
+        <img src="src\assets\Secondaire\4462832.jpg" className="d-block w-100" alt="..."/>
+        <h2 className='text-success' style={{textAlign:"center"}}>Statistiques</h2>
+          <p style={{textAlign:"center"}}>Statistiques des données d'analyses par patient et sur les données du système</p>
+        </div>
       </div>
   </div>
 
